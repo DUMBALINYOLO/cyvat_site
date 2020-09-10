c8bfc3211363821fddb239fca75d4e8771880788
Dumbalinyolo <baridzimaximillem@gmail.com>
2020-09-10 20:25:08 +0200

    'lik'

---
+++ reactify-ui/build/precache-manifest.4fe81f603c7c179c09149b7e158b914b.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90afad7d4316a0e775b26ac9d3196709",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f077d737cf0162a8548",
+    "url": "/static/css/2.eed5f645.chunk.css"
+  },
+  {
+    "revision": "627e2c7a6bf1957f4966",
+    "url": "/static/css/main.7bae6ecd.chunk.css"
+  },
+  {
+    "revision": "9f077d737cf0162a8548",
+    "url": "/static/js/2.d436b1b2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.d436b1b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "627e2c7a6bf1957f4966",
+    "url": "/static/js/main.517c2b1c.chunk.js"
+  },
+  {
+    "revision": "a1146b57aa8cd641b4e3",
+    "url": "/static/js/runtime-main.a7b7d35c.js"
+  },
+  {
+    "revision": "c7a33805ffda0d32bd2a9904c8b02750",
+    "url": "/static/media/color.c7a33805.png"
+  },
+  {
+    "revision": "0bb7e0748f6a2bfbb9d5222da02bbe92",
+    "url": "/static/media/cyv1.0bb7e074.jpg"
+  },
+  {
+    "revision": "20c7ed6c7ff013c027264479827dfa64",
+    "url": "/static/media/cyv2.20c7ed6c.jpg"
+  },
+  {
+    "revision": "0bd04f20c0b71948e794dc09b227a9c7",
+    "url": "/static/media/cyv3.0bd04f20.jpg"
+  },
+  {
+    "revision": "5f881573b035f183ede9a6c9bbd70e24",
+    "url": "/static/media/cyv4.5f881573.jpg"
+  },
+  {
+    "revision": "567f57385ea3dde2c9aec797d07850d2",
+    "url": "/static/media/line.567f5738.gif"
+  },
+  {
+    "revision": "177cc92d2e8027712a8c1724abd272cd",
+    "url": "/static/media/open-sans-v15-latin-300.177cc92d.ttf"
+  },
+  {
+    "revision": "27ef0b062b2e221df16f3bbd97c2dca8",
+    "url": "/static/media/open-sans-v15-latin-300.27ef0b06.svg"
+  },
+  {
+    "revision": "521d17bc9f3526c690e8ada6eee55bec",
+    "url": "/static/media/open-sans-v15-latin-300.521d17bc.woff"
+  },
+  {
+    "revision": "60c866748ff15f5b347fdba64596b1b1",
+    "url": "/static/media/open-sans-v15-latin-300.60c86674.woff2"
+  },
+  {
+    "revision": "76b56857ebbae3a5a689f213feb11af0",
+    "url": "/static/media/open-sans-v15-latin-300.76b56857.eot"
+  },
+  {
+    "revision": "148a6749baa5f658a45183ddb5ee159f",
+    "url": "/static/media/open-sans-v15-latin-700.148a6749.eot"
+  },
+  {
+    "revision": "2e00b2635b51ba336b4b67a5d0bc03c7",
+    "url": "/static/media/open-sans-v15-latin-700.2e00b263.svg"
+  },
+  {
+    "revision": "623e3205570002af47fc2b88f9335d19",
+    "url": "/static/media/open-sans-v15-latin-700.623e3205.woff"
+  },
+  {
+    "revision": "7e08cc656863d52bcb5cd34805ac605b",
+    "url": "/static/media/open-sans-v15-latin-700.7e08cc65.ttf"
+  },
+  {
+    "revision": "d08c09f2f169f4a6edbcf8b8d1636cb4",
+    "url": "/static/media/open-sans-v15-latin-700.d08c09f2.woff2"
+  },
+  {
+    "revision": "7aab4c13671282c90669eb6a10357e41",
+    "url": "/static/media/open-sans-v15-latin-regular.7aab4c13.svg"
+  },
+  {
+    "revision": "9dce7f01715340861bdb57318e2f3fdc",
+    "url": "/static/media/open-sans-v15-latin-regular.9dce7f01.eot"
+  },
+  {
+    "revision": "bf2d0783515b7d75c35bde69e01b3135",
+    "url": "/static/media/open-sans-v15-latin-regular.bf2d0783.woff"
+  },
+  {
+    "revision": "c045b73d86803686f4cd1cc3f9ceba59",
+    "url": "/static/media/open-sans-v15-latin-regular.c045b73d.ttf"
+  },
+  {
+    "revision": "cffb686d7d2f4682df8342bd4d276e09",
+    "url": "/static/media/open-sans-v15-latin-regular.cffb686d.woff2"
+  },
+  {
+    "revision": "159263bf4976843830365d52d1f94fba",
+    "url": "/static/media/primeicons.159263bf.eot"
+  },
+  {
+    "revision": "2e3a6b48408add8bb604d0b4e820a3b5",
+    "url": "/static/media/primeicons.2e3a6b48.ttf"
+  },
+  {
+    "revision": "4e915114c908962a6ff2d505c2a4816b",
+    "url": "/static/media/primeicons.4e915114.svg"
+  },
+  {
+    "revision": "79432094e4838b36d974b9d8c57d22e2",
+    "url": "/static/media/primeicons.79432094.woff"
+  }
+]);
